743ffffb73ad4f0e692fd0d9cdbf3da1a2cd27d6
Oyungerel Bernstein <oogiisar@gmail.com>
2019-09-25 22:13:07 -0700

    added multi state search

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
 'use strict';
 
-
-
-
-
-
 function displayResults(responseJson, maxResults) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.data.length & i<maxResults; i++){
-    // for each video object in the items 
+    console.log(responseJson);
+    // iterate through the data array
+    for (let i = 0; i < responseJson.data.length & i<maxResults; i++){
     //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
+    //list with the fullName, description, url
     $('#results-list').append(
       `<h3>${responseJson.data[i].fullName}</h3>
       <p>${responseJson.data[i].description}</p>
       <p>${responseJson.data[i].url}</p>`
     )};
-  //display the results section  
-  $('#results').removeClass('hidden');
+    //display the results section  
+    $('#results').removeClass('hidden');
 };
 
-  
- function getParksInfo(searchURL, maxResults=10) {
-  
-    console.log(searchURL);
-  
-  
-  
-  
-  fetch(searchURL)
-    .then(response => {
+function getParksInfo(searchURL, maxResults=10) {
+  for(let i = 0; i < searchURL.length; i++){
+    console.log(`https://developer.nps.gov/api/v1/parks?stateCode=${searchURL[i]}&api_key=EeQVlcDqfV6U3IonkVKltTzZz45LICHii2vRdujG`);
+    fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${searchURL[i]}&api_key=EeQVlcDqfV6U3IonkVKltTzZz45LICHii2vRdujG`)
+      .then(response => {
       if (response.ok) {
         return response.json();
       }
@@ -43,18 +29,19 @@ function displayResults(responseJson, maxResults) {
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
+  }
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
+    $('#results-list').empty();
     const searchTerm = $('select').val();
     const maxResults = $('#js-max-results').val();
-    const searchURL = `https://developer.nps.gov/api/v1/parks?stateCode=${searchTerm}&api_key=EeQVlcDqfV6U3IonkVKltTzZz45LICHii2vRdujG`;
-    getParksInfo(searchURL, maxResults);
-  });
+    console.log(searchTerm);
+    getParksInfo(searchTerm, maxResults);
+    });
 }
-
 $(watchForm);
 
 
